9fb6c9fc155108dadea50de87b46bd2c4208640f
Cory Stever <cstever@hotmail.com>
2024-11-05 19:09:23 -0600

    First Commit

---
+++ headers/my-queue.h
@@ -0,0 +1,35 @@
+#ifndef MY_QUEUE_H
+#define MY_QUEUE_H
+#include <iostream>
+
+using namespace std;
+
+template<typename Item_Type>
+class My_Queue {
+private:
+    struct Node {
+        Item_Type data;
+        Node* next;
+
+        Node(const Item_Type& item_data, Node* next_node = nullptr);
+    };
+
+    Node* head;
+    Node* tail;
+    size_t num_items;
+
+public:
+    My_Queue();
+    ~My_Queue();
+
+    void push(const Item_Type& item);
+    void pop();
+    const Item_Type& front() const;
+    int size();
+    bool empty();
+    void move_to_rear();
+    void display();
+};
+
+
+#endif
